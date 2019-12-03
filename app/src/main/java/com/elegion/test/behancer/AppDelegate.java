bdd82c4aa02f5845823ff5b29a62f3634e0ed210
savelgmx <savel@gmx.com>
2019-12-03 16:46:30 +0700

    TODO Исправить  error: cannot find symbol         MemberInjectorRegistryLocator

--- app/src/main/java/com/elegion/test/behancer/AppDelegate.java
+++ app/src/main/java/com/elegion/test/behancer/AppDelegate.java
@@ -5,6 +5,15 @@ import android.arch.persistence.room.Room;
 
 import com.elegion.test.behancer.data.Storage;
 import com.elegion.test.behancer.data.database.BehanceDatabase;
+import com.elegion.test.behancer.di.AppModule;
+import com.elegion.test.behancer.di.NetworkModule;
+
+import toothpick.Scope;
+import toothpick.Toothpick;
+import toothpick.configuration.Configuration;
+import toothpick.registries.FactoryRegistryLocator;
+import toothpick.registries.MemberInjectorRegistryLocator;
+import toothpick.smoothie.module.SmoothieApplicationModule;
 
 /**
  * Created by Vladislav Falzan.
@@ -13,6 +22,7 @@ import com.elegion.test.behancer.data.database.BehanceDatabase;
 public class AppDelegate extends Application {
 
     private Storage mStorage;
+    private static Scope sAppScope;
 
     @Override
     public void onCreate() {
@@ -23,6 +33,18 @@ public class AppDelegate extends Application {
                 .build();
 
         mStorage = new Storage(database.getBehanceDao());
+        //==========Tooth Prick==========================
+        Toothpick.setConfiguration(Configuration.forProduction().disableReflection());
+        MemberInjectorRegistryLocator.setRootRegistry(new com.elegion.test.behancer.MemberInjectorRegistry());
+        FactoryRegistryLocator.setRootRegistry(new com.elegion.test.behancer.FactoryRegistry());
+
+        sAppScope = Toothpick.openScope(AppDelegate.class);
+        sAppScope.installModules(new SmoothieApplicationModule(this), new NetworkModule(), new AppModule(this));
+
+        //===================================================
+    }
+    public static Scope getAppScope() {
+        return sAppScope;
     }
 
     public Storage getStorage() {

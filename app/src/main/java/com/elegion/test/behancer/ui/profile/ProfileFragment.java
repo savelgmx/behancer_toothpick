aec4d1872a0f92c88f705f9e4bbf66ec12c3a773
savelgmx <savel@gmx.com>
2019-12-08 18:15:46 +0700

    TODO Сделать отображение профиля пользвателя инжектить Storage где надо Сервис для сетевых запросов (BehanceApi) сейчас доступен через статический метод ApiUtils.getApiService(). Его также нужно инжектить во ViewModel

--- app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.elegion.test.behancer.ui.profile;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,136 +11,67 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.elegion.test.behancer.R;
-import com.elegion.test.behancer.common.RefreshOwner;
-import com.elegion.test.behancer.common.Refreshable;
 import com.elegion.test.behancer.data.Storage;
-import com.elegion.test.behancer.data.model.user.User;
-import com.elegion.test.behancer.utils.ApiUtils;
-import com.elegion.test.behancer.utils.DateUtils;
-import com.squareup.picasso.Picasso;
+import com.elegion.test.behancer.databinding.ProfileBinding;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+import javax.inject.Inject;
 
 /**
  * Created by Vladislav Falzan.
  */
 
-public class ProfileFragment extends Fragment implements Refreshable {
-
-    public static final String PROFILE_KEY = "PROFILE_KEY";
-
-    private RefreshOwner mRefreshOwner;
-    private View mErrorView;
-    private View mProfileView;
-    private String mUsername;
-    private Storage mStorage;
-    private Disposable mDisposable;
-
-    private ImageView mProfileImage;
-    private TextView mProfileName;
-    private TextView mProfileCreatedOn;
-    private TextView mProfileLocation;
-
-    public static ProfileFragment newInstance(Bundle args) {
-        ProfileFragment fragment = new ProfileFragment();
-        fragment.setArguments(args);
-
-        return fragment;
-    }
 
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        mStorage = context instanceof Storage.StorageOwner ? ((Storage.StorageOwner) context).obtainStorage() : null;
-        mRefreshOwner = context instanceof RefreshOwner ? (RefreshOwner) context : null;
-    }
+    public class ProfileFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fr_profile, container, false);
-    }
+        public static final String PROFILE_KEY = "PROFILE_KEY";
+        private String mUsername;
+       /// private
+    @Inject
+       ProfileViewModel mProfileViewModel;
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        mErrorView = view.findViewById(R.id.errorView);
-        mProfileView = view.findViewById(R.id.view_profile);
 
-        mProfileImage = view.findViewById(R.id.iv_profile);
-        mProfileName = view.findViewById(R.id.tv_display_name_details);
-        mProfileCreatedOn = view.findViewById(R.id.tv_created_on_details);
-        mProfileLocation = view.findViewById(R.id.tv_location_details);
-    }
+        public static com.elegion.test.behancer.ui.profile.ProfileFragment newInstance(Bundle args) {
+            com.elegion.test.behancer.ui.profile.ProfileFragment fragment = new com.elegion.test.behancer.ui.profile.ProfileFragment();
+            fragment.setArguments(args);
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        if (getArguments() != null) {
-            mUsername = getArguments().getString(PROFILE_KEY);
+            return fragment;
         }
 
-        if (getActivity() != null) {
-            getActivity().setTitle(mUsername);
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            mUsername=getArguments().getString(PROFILE_KEY);
+
+
+            if (context instanceof Storage.StorageOwner) {
+                Storage storage = ((Storage.StorageOwner) context).obtainStorage();
+                //   mProfileViewModel = new ProfileViewModel(storage,mUsername);
+
+                ViewModelProvider.Factory factory = new ViewModelProvider.Factory() {
+                    @NonNull
+                    @Override
+                    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                        return (T) new ProfileViewModel(storage,mUsername);
+                    }
+                };
+                mProfileViewModel = ViewModelProviders.of(this, factory).get(ProfileViewModel.class);
+
+
+            }
+
         }
 
-        mProfileView.setVisibility(View.VISIBLE);
 
-        onRefreshData();
-    }
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            ProfileBinding binding = ProfileBinding.inflate(inflater,container,false);
+            binding.setPm(mProfileViewModel);
+            return binding.getRoot();
 
-    @Override
-    public void onRefreshData() {
-        getProfile();
-    }
-
-    private void getProfile() {
-        mDisposable = ApiUtils.getApiService().getUserInfo(mUsername)
-                .subscribeOn(Schedulers.io())
-                .doOnSuccess(response -> mStorage.insertUser(response))
-                .onErrorReturn(throwable ->
-                        ApiUtils.NETWORK_EXCEPTIONS.contains(throwable.getClass()) ?
-                                mStorage.getUser(mUsername) :
-                                null)
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe(disposable -> mRefreshOwner.setRefreshState(true))
-                .doFinally(() -> mRefreshOwner.setRefreshState(false))
-                .subscribe(
-                        response -> {
-                            mErrorView.setVisibility(View.GONE);
-                            mProfileView.setVisibility(View.VISIBLE);
-                            bind(response.getUser());
-                        },
-                        throwable -> {
-                            mErrorView.setVisibility(View.VISIBLE);
-                            mProfileView.setVisibility(View.GONE);
-                        });
-    }
-
-    private void bind(User user) {
-        Picasso.with(getContext())
-                .load(user.getImage().getPhotoUrl())
-                .fit()
-                .into(mProfileImage);
-        mProfileName.setText(user.getDisplayName());
-        mProfileCreatedOn.setText(DateUtils.format(user.getCreatedOn()));
-        mProfileLocation.setText(user.getLocation());
-    }
-
-    @Override
-    public void onDetach() {
-        mStorage = null;
-        mRefreshOwner = null;
-        if (mDisposable != null) {
-            mDisposable.dispose();
         }
-        super.onDetach();
+
     }
-}
